9d7a8b65ea97209c5889e2454895ca1a8116d0a3
nykci <150587687+yumal-nykci@users.noreply.github.com>
2025-01-11 10:48:13 +0900

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,10 @@
+　...
+  "start_url": "[GitHub Pages のURL]",
+  "url": "[GitHub Pages のURL]",
+  "display": "standalone",
+  "theme_color": "#000000",
+  "background_color": "#ffffff",
+  "screenshots": [],
+  "orientation": "portrait",
+  "lang":"ja"
+}
