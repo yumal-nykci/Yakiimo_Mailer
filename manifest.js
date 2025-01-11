581d6a4ae7478bd7fb0d55acb1cd3b592718755d
nykci <150587687+yumal-nykci@users.noreply.github.com>
2025-01-11 12:06:56 +0900

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,10 +1,13 @@
-　...
-  "start_url": "[GitHub Pages のURL]",
-  "url": "[GitHub Pages のURL]",
-  "display": "standalone",
-  "theme_color": "#000000",
-  "background_color": "#ffffff",
-  "screenshots": [],
-  "orientation": "portrait",
-  "lang":"ja"
+{
+  "name": "Yakiimo_Mailer",
+  "short_name": "imomailer",
+  "background_color": "#000000",
+  "icons": [{
+      "src": "/Yakiimo_Mailer/images/icon.png",
+      "sizes": "256x256",
+      "type": "image/png"
+    }],
+  "start_url": "/Yakiimo_Mailer/",
+  "scope": "/Yakiimo_Mailer/",
+  "display": "standalone"
 }
