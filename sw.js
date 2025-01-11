718ea8c836c71d0b907fed16602d0b7f1ddab472
nykci <150587687+yumal-nykci@users.noreply.github.com>
2025-01-11 11:03:06 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+var CACHE_NAME = 'pwa-sample-caches';
+var urlsToCache = [
+	'Yakiimo_Mailer',
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches
+			.open(CACHE_NAME)
+			.then(function(cache) {
+				return cache.addAll(urlsToCache);
+			})
+	);
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches
+			.match(event.request)
+			.then(function(response) {
+				return response ? response : fetch(event.request);
+			})
+	);
+});
